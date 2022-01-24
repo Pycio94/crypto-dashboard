03ea6cc9b8d058badabb19d8fb23df36bc7e2809
Piotr Pytka <40126766+Pycio94@users.noreply.github.com>
2022-01-25 00:48:51 +0100

    update
    api added

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,18 +1,42 @@
 import { useState } from "react";
 import ExchangeRate from "./ExchangeRate";
-
+import axios from "axios";
 
 const CurrencyConverter = () => {
     const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA']
     const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC')
     const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('BTC')
+    const [amount, setAmount] = useState(1)
+    const [exchangeRate, setExchangeRate] = useState(0)
+    const [result, setResult] = useState(0)
 
-    console.log(chosenSecondaryCurrency)
+    console.log(amount)
 
+    const convert = () => {
 
+        const options = {
+            method: 'GET',
+            url: 'https://alpha-vantage.p.rapidapi.com/query',
+            params: {from_currency: chosenPrimaryCurrency, function: 'CURRENCY_EXCHANGE_RATE', to_currency: chosenSecondaryCurrency},
+            headers: {
+                'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
+                'x-rapidapi-key': 'd767b9a65emsh1fd71fa7a5eb899p11006cjsna58fba31fc53'
+            }
+        }
+
+        axios.request(options).then((response) => {
+            console.log(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+            setExchangeRate(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+            setResult(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] * amount)
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
+
+    console.log(exchangeRate)
     return (
         <div className="currency-converter">
-        <h2>CurrencyConverter</h2>
+        <h2>Currency Converter</h2>
 
         <div className="input-box">
 
@@ -24,7 +48,8 @@ const CurrencyConverter = () => {
                         <input
                             type="number"
                             name="currency-amount-1"
-                            value={""}
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
                         />
                     </td>
                     <td>
@@ -33,7 +58,6 @@ const CurrencyConverter = () => {
                             name="currency-option-1"
                             className="currency-options"
                             onChange={(e) => setChosenPrimaryCurrency(e.target.value)}
-
                         >
                             {currencies.map( (currency, _index) => <option key={_index}>{currency}</option>)}
 
@@ -45,9 +69,9 @@ const CurrencyConverter = () => {
                     <td>Secondary Currency:</td>
                     <td>
                         <input
-                            type="number"
                             name="currency-amount-2"
-                            value={""}
+                            value={result}
+                            disabled={true}
                         />
                     </td>
                     <td>
@@ -56,7 +80,6 @@ const CurrencyConverter = () => {
                             name="currency-option-2"
                             className="currency-options"
                             onChange={(e) => setChosenSecondaryCurrency(e.target.value)}
-
                         >
                             {currencies.map( (currency, _index) => <option key={_index}>{currency}</option>)}
 
@@ -67,11 +90,17 @@ const CurrencyConverter = () => {
                 </tbody>
             </table>
 
+            <button id="convert-button" onClick={convert}>Convert</button>
+
         </div>
 
-
-        <ExchangeRate/>
-    </div>)
+            <ExchangeRate
+                exchangeRate={exchangeRate}
+                chosenPrimaryCurrency={chosenPrimaryCurrency}
+                chosenSecondaryCurrency={chosenSecondaryCurrency}
+            />
+        </div>
+    )
 }
 
 export default CurrencyConverter
